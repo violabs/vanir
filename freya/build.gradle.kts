8415e91038d973155f129abd72f91b778a0daf21
Josh Stallnick <violabs.software@gmail.com>
2023-05-15 01:32:47 -0500

    Fix the builds up and added some testing

--- freya/build.gradle.kts
+++ freya/build.gradle.kts
@@ -1,23 +1,23 @@
-plugins {
-    id("org.springframework.boot")
-
-    kotlin("jvm")
-    kotlin("plugin.spring")
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-//	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-//	implementation("org.apache.kafka:kafka-streams")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
-}
+//plugins {
+//    id("org.springframework.boot")
+//
+//    kotlin("jvm")
+//    kotlin("plugin.spring")
+//}
+//
+//dependencies {
+//	implementation("org.springframework.boot:spring-boot-starter-actuator")
+////	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+//	implementation("org.springframework.boot:spring-boot-starter-webflux")
+//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+////	implementation("org.apache.kafka:kafka-streams")
+//	implementation("org.jetbrains.kotlin:kotlin-reflect")
+//	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+//	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	testImplementation("io.projectreactor:reactor-test")
+//}
 
 //tasks.withType<Test> {
 //	systemProperty("spring.profiles.active", "test")
