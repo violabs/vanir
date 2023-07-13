8ee3991b060d0f463bb67dddd2fdd824bba4c373
Josh Stallnick <violabs.software@gmail.com>
2023-07-13 09:27:49 -0500

    Wired in the create endpoint for events

--- freya/src/test/kotlin/io/violabs/freya/message/UserProducerIntegrationTest.kt
+++ freya/src/test/kotlin/io/violabs/freya/message/UserProducerIntegrationTest.kt
@@ -4,9 +4,7 @@ import io.violabs.core.TestUtils
 import io.violabs.core.domain.UserMessage
 import io.violabs.freya.KafkaTestConfig
 import io.violabs.freya.domain.AppUser
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeoutOrNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -22,10 +20,9 @@ class UserProducerIntegrationTest(
     @Test
     fun `should send user data to kafka`() = runBlocking {
         val user = AppUser(1, "test", "test", "test", "test")
-        val message = UserMessage(1, "localhost:8080/user/1", UserMessage.Type.USER_CREATED)
+        val message = UserMessage(1, "http://localhost:8080/api/users/1", UserMessage.Type.USER_CREATED)
         userProducer.sendUserData(user)
-        delay(2000)
-        val receivedMessage = withTimeoutOrNull(15_000) { userKafkaConsumer.consume() }
+        val receivedMessage = userKafkaConsumer.consume()
         TestUtils.assertEquals(message, receivedMessage)
     }
 }
